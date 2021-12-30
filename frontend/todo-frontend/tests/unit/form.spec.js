7ca95e8ed8121984a1e903762d3fe3f7207b5123
wajishah541 <syhu@ciklum.com>
2021-12-30 22:15:51 +0500

    commit

--- frontend/todo-frontend/tests/unit/form.spec.js
+++ frontend/todo-frontend/tests/unit/form.spec.js
@@ -1,5 +1,7 @@
 import { Mount } from '@vue/test-utils'
 import Todo from '@/components/Todo'
+import axios from 'axios'
+import mockAxios from 'jest-mock-axios'
 //Acceptance Test
 describe('The todo.vue component', () => {
   it('Enables user to add todo ', () => {
@@ -19,7 +21,7 @@ describe('The todo.vue component', () => {
       }
     ]
   })
-  const result = await insertTodo()
+  const result = insertTodo()
   expect(axios.post).toHaveBeenCalledWith(`${postTodoUrl}/handlerequest`)
   expect(result.data).toEqual(tasks)
    })
