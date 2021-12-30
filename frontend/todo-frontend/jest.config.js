e82cb8e0a99eb2c09fc348ab8b371fa9f717ae26
wajishah541 <syhu@ciklum.com>
2021-12-30 21:54:46 +0500

    initial commit

--- frontend/todo-frontend/jest.config.js
+++ frontend/todo-frontend/jest.config.js
@@ -1,4 +1,26 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest'
   
+},
+{
+  "jest": {
+    "moduleFileExtensions": [
+      "js",
+      "json",
+      // tell Jest to handle `*.vue` files
+      "vue"
+    ],
+    "transform": {
+      // process `*.vue` files with `vue-jest`
+      ".*\\.(vue)$": "vue-jest"
+    }
+  }
+},
+{
+  "jest": {
+    "transform": {
+      // process `*.js` files with `babel-jest`
+      ".*\\.(js)$": "babel-jest"
+    }
+  }
 }
--- frontend/todo-frontend/tests/unit/form.spec.js
+++ frontend/todo-frontend/tests/unit/form.spec.js
@@ -1,11 +1,26 @@
-import { shallowMount } from '@vue/test-utils'
-
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+import { Mount } from '@vue/test-utils'
+import Todo from '@/components/Todo'
+//Acceptance Test
+describe('The todo.vue component', () => {
+  it('Enables user to add todo ', () => {
+    const wrapper = Mount(Todo,{
+    props:{
+      title: 'Todo-App'
+    }
   })
+  const tasks = [
+    { ID: 1 , content: 'go for a walk '
+    }
+  ]
+  axios.post = jest.fn().mockResolvedValue({
+    data:[
+      {
+        ID: 1 , content: 'go for a walk '
+      }
+    ]
+  })
+  const result = await insertTodo()
+  expect(axios.post).toHaveBeenCalledWith(`${postTodoUrl}/handlerequest`)
+  expect(result.data).toEqual(tasks)
+   })
 })
